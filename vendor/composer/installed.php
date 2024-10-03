0d4d732b1552cea151ad28a742a74f12a33831af
Dinu Senal Sendanayake <dinusenal8@gmail.com>
2024-10-03 19:45:07 +1000

    feat: added Stripe PHP SDK

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '13ec977d745828ec1eba9f33fde56365c882cbb3',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '13ec977d745828ec1eba9f33fde56365c882cbb3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v16.0.0',
+            'version' => '16.0.0.0',
+            'reference' => '7a0b0283d492b66474ef9aa45a7acc938662b580',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
